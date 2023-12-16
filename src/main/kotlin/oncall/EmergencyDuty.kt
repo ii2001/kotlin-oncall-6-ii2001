5a4f7564706aaf439510f28a32313add64be833a
leeyeongjae <xingxing2001@cau.ac.kr>
2023-12-16 14:03:45 +0900

    feat: 요일 계산 함수 제작

--- src/main/kotlin/oncall/EmergencyDuty.kt
+++ src/main/kotlin/oncall/EmergencyDuty.kt
@@ -3,39 +3,76 @@ package oncall
 class EmergencyDuty {
     fun assignDuty(month: Int, startDay: String, weekdayDuties: List<String>, weekendDuties: List<String>): List<String> {
         val schedule = mutableListOf<String>()
-        val calendar = createCalendar(month, startDay)
+        val totalDays = getDaysInMonth(month)
+        val dayOffset = getDayOffset(startDay)
 
-        val weekdayIterator = weekdayDuties.iterator().withCycle()
-        val weekendIterator = weekendDuties.iterator().withCycle()
+        var weekdayIndex = 0
+        var weekendIndex = 0
 
-        for (date in calendar) {
-            val isWeekend = date.isWeekendOrHoliday()
-            val nextDuty = if (isWeekend) weekendIterator.next() else weekdayIterator.next()
+        for (day in 1..totalDays) {
+            val dayOfWeek = (day + dayOffset - 1) % 7
+            val isWeekend = dayOfWeek == 5 || dayOfWeek == 6 // 토요일(5) 또는 일요일(6)인 경우
 
-            // 연속 근무 방지 로직
-            if (!schedule.isEmpty() && schedule.last().endsWith(nextDuty)) {
+            val currentDuty = if (isWeekend) weekendDuties[weekendIndex % weekendDuties.size] else weekdayDuties[weekdayIndex % weekdayDuties.size]
+
+            // 연속 근무 방지
+            if (schedule.isNotEmpty() && schedule.last().endsWith(currentDuty)) {
                 if (isWeekend) {
-                    schedule.add(date to weekendIterator.next())
+                    weekendIndex++
                 } else {
-                    schedule.add(date to weekdayIterator.next())
+                    weekdayIndex++
                 }
+
+                continue
+            }
+
+            val dateString = "${month}월 ${day}일 ${convertDayOfWeekToString(dayOfWeek)}"
+            schedule.add("$dateString $currentDuty")
+
+            if (isWeekend) {
+                weekendIndex++
             } else {
-                schedule.add(date to nextDuty)
+                weekdayIndex++
             }
         }
 
-        return schedule.map { (date, duty) -> "$date $duty" }
+        return schedule
     }
 
-    private fun createCalendar(month: Int, startDay: String): List<String> {
-        // 주어진 월과 시작 요일에 따라 날짜 목록 생성
+    private fun getDaysInMonth(month: Int): Int {
+        // 2월은 28일, 나머지는 30 또는 31일로 계산
+        return when (month) {
+            2 -> 28
+            4, 6, 9, 11 -> 30
+            else -> 31
+        }
     }
 
-    private fun String.isWeekendOrHoliday(): Boolean {
-        // 주말 또는 공휴일 여부 확인
+    private fun getDayOffset(startDay: String): Int {
+        // 요일에 따라 숫자로 변환 (월요일: 0, 화요일: 1, ..., 일요일: 6)
+        return when (startDay) {
+            "월" -> 0
+            "화" -> 1
+            "수" -> 2
+            "목" -> 3
+            "금" -> 4
+            "토" -> 5
+            "일" -> 6
+            else -> throw IllegalArgumentException("잘못된 시작 요일")
+        }
     }
 
-    private fun <T> Iterator<T>.withCycle(): Iterator<T> {
-        // 순환 반복자 생성
+    private fun convertDayOfWeekToString(dayOfWeek: Int): String {
+        // 숫자를 요일 문자열로 변환
+        return when (dayOfWeek) {
+            0 -> "월"
+            1 -> "화"
+            2 -> "수"
+            3 -> "목"
+            4 -> "금"
+            5 -> "토"
+            6 -> "일"
+            else -> throw IllegalArgumentException("잘못된 요일")
+        }
     }
 }
