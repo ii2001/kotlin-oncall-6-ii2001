0b4217a221d5c3daa2e29a852aa2bde251bbce84
leeyeongjae <xingxing2001@cau.ac.kr>
2023-12-16 17:14:43 +0900

    refect: DutyController.kt 리펙토링 완료

--- src/main/kotlin/oncall/DutyController.kt
+++ src/main/kotlin/oncall/DutyController.kt
@@ -3,54 +3,41 @@ package oncall
 class DutyController {
     private val model = EmergencyDuty()
     private val view = DutyScheduleView()
-
     fun start() {
-        var month: Int
-        var startDay: String
-        var weekdayDutyRoster: MutableList<String>
-        var weekendDutyRoster: MutableList<String>
+        val (month, startDay) = getMonthAndStartDay()
+        val (weekdayDutyRoster, weekendDutyRoster) = getDutyRosters()
+        val schedule = model.assignDuty(month, startDay, weekdayDutyRoster, weekendDutyRoster)
+        view.displaySchedule(schedule)
+    }
 
-        // 월과 시작 요일 입력 받기
+    // 월과 시작 요일 입력 받기
+    private fun getMonthAndStartDay(): Pair<Int, String> {
         while (true) {
             try {
                 view.displayMessage("비상 근무를 배정할 월과 시작 요일을 입력하세요> ")
-                val monthAndStartDay = parseMonthAndStartDay(view.getInput())
-                month = monthAndStartDay.first
-                startDay = monthAndStartDay.second
-                break
+                return parseMonthAndStartDay(view.getInput())
             } catch (e: IllegalArgumentException) {
-                // 유효하지 않은 입력 처리
                 view.displayError("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.")
             }
         }
+    }
 
-// 평일 근무자 순서와 휴일 근무자 순서를 입력 받기
+    // 평일 근무자 순서와 휴일 근무자 순서를 입력 받기
+    private fun getDutyRosters(): Pair<MutableList<String>, MutableList<String>> {
         while (true) {
             try {
                 view.displayMessage("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ")
-                weekdayDutyRoster = parseDutyRoster(view.getInput())
-
+                val weekdayDutyRoster = parseDutyRoster(view.getInput())
                 view.displayMessage("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ")
-                weekendDutyRoster = parseDutyRoster(view.getInput())
-
+                val weekendDutyRoster = parseDutyRoster(view.getInput())
                 if (weekdayDutyRoster.isNotEmpty() && weekendDutyRoster.isNotEmpty()) {
-                    break
+                    return weekdayDutyRoster to weekendDutyRoster
                 } else {
                     view.displayError("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.")
                 }
             } catch (e: IllegalArgumentException) {
-                // 유효하지 않은 입력 처리
                 view.displayError("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.")
-            }
-        }
-
-
-        // 근무 스케줄 생성
-        val schedule = model.assignDuty(month, startDay, weekdayDutyRoster, weekendDutyRoster)
-
-        // 스케줄 표시
-        view.displaySchedule(schedule)
-    }
+            } } }
 
     private fun parseMonthAndStartDay(input: String): Pair<Int, String> {
         val validDays = setOf("월", "화", "수", "목", "금", "토", "일")
@@ -65,14 +52,11 @@ class DutyController {
 
     private fun parseDutyRoster(input: String): MutableList<String> {
         val roster = input.split(",").map { it.trim() }
-
         // 중복된 닉네임 확인
         val uniqueRoster = roster.distinct()
-
         if (roster.size != uniqueRoster.size || roster.size !in 5..35 || roster.any { it.length >= 6 }) {
             throw IllegalArgumentException("사원 수가 5명에서 35명 사이이며, 사원 닉네임은 6글자 미만이고 중복되지 않아야 합니다.")
         }
-
         return uniqueRoster.toMutableList()
     }
 }
